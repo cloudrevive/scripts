fb405f36f5d3bc81e01f34c5351cd521f41cb1c9
yuli <stremovsky@gmail.com>
2024-07-16 18:18:36 +0300

    fix scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$PWD/$0")"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "0 0 * * * root $SCRIPT_DIR/update.sh" > /etc/cron.d/cloudrevive
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$PWD/$0")"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 git pull
