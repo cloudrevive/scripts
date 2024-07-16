c4242151a15776db11e8353cc2ca0035794fe74b
yuli <stremovsky@gmail.com>
2024-07-16 09:26:38 +0300

    docker kill

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+docker kill agent || true
+docker rm agent || true
 docker pull registry.metal8.cloud/cloudrevive-agent:1.0.0
 docker run -p 80:5000 -d --restart unless-stopped --name agent registry.metal8.cloud/cloudrevive-agent:1.0.0
