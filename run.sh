a3dfd4e56a85b58589fdcc8cff85f7961a5379b9
yuli <stremovsky@gmail.com>
2024-07-18 23:51:27 +0300

    add ssl listener

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE="certs/erver.crt"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FILE="certs/server.crt"
 
 if [ -e "$FILE" ]; then
     EXPIRATION_DATE=$(openssl x509 -enddate -noout -in certs/server.crt | cut -d= -f2)
@@ -18,4 +19,4 @@ fi
 docker kill agent || true
 docker rm agent || true
 docker pull registry.metal8.cloud/cloudrevive-agent:1.0.0
-docker run -p 80:5000 -d --restart unless-stopped --name agent registry.metal8.cloud/cloudrevive-agent:1.0.0
+docker run -p 80:80 -p 443:443 -v $SCRIPT_DIR/certs:/app/certs -d --restart unless-stopped --name agent registry.metal8.cloud/cloudrevive-agent:1.0.0
